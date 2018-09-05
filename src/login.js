2bebd9a52eadd764f9be284adc42d282fff7312b
Vitor Santos <vitor.santos@sysmap.com.br>
2018-09-05 13:52:25 -0300

    Atualizações de layout

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card , Row, Col, Form, Input, Icon, Button} from 'antd';
+import { Card , Form, Input, Icon, Button} from 'antd';
 import './assets/css/login.css';
 import 'antd/dist/antd.css';
 
@@ -21,32 +21,28 @@ class Login extends Component {
         const { form } = this.props;
         const { getFieldDecorator } = form;
         return (
-            <div className="login">
-                <Row className="row"> 
-                    <Col span="10" offset="7">
-                        <Card title="Plutus" headStyle={{textAlign:"center"}}>
-                            <Form onSubmit={ev => this.sendLogin(ev)}>
-                                <FormItem>
-                                    {getFieldDecorator('userName', {
-                                        rules: [{ required: true, message: 'Por favor coloque seu email!' }],
-                                    })(
-                                        <Input placeholder="Seu email" prefix={<Icon type="user"/>}/>
-                                    )}
-                                </FormItem>
-                                <FormItem>
-                                    {getFieldDecorator('passWord', {
-                                        rules: [{ required: true, message: 'Por favor coloque sua senha!' }],
-                                    })(
-                                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Senha" />
-                                    )}    
-                                </FormItem>
-                                <FormItem>
-                                    <Button htmlType="submit" block style={{background: 'rgb(39, 178, 38)', color: 'rgb(253, 254, 248)'}}>Entrar</Button>
-                                </FormItem>
-                            </Form>
-                        </Card>
-                    </Col> 
-                </Row>                                  
+            <div className="plutus-container-login">
+                <Card title="Plutus" headStyle={{textAlign:"center"}} className="plutus-login-form">
+                    <Form onSubmit={ev => this.sendLogin(ev)}>
+                        <FormItem>
+                            {getFieldDecorator('userName', {
+                                rules: [{ required: true, message: 'Por favor coloque seu email!' }],
+                            })(
+                                <Input placeholder="Seu email" prefix={<Icon type="user"/>}/>
+                            )}
+                        </FormItem>
+                        <FormItem>
+                            {getFieldDecorator('passWord', {
+                                rules: [{ required: true, message: 'Por favor coloque sua senha!' }],
+                            })(
+                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Senha" />
+                            )}    
+                        </FormItem>
+                        <FormItem>
+                            <Button htmlType="submit" block style={{background: 'rgb(39, 178, 38)', color: 'rgb(253, 254, 248)'}}>Entrar</Button>
+                        </FormItem>
+                    </Form>
+                </Card>
             </div>
         ) 
     }
