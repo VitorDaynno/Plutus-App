ce63e06c7ceb9272353643c191dfc1dd7ccf053a
VitorDaynno <vitordaynno@gmail.com>
2018-11-21 23:39:56 -0200

    Menu lateral

--- src/components/mainMenu.jsx
+++ src/components/mainMenu.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Menu, Icon } from 'antd';
+import { NavLink } from 'react-router-dom';
 
 class MainMenu extends Component {
-
+    
     render() {
+        
         return (
             <div>
                 <Menu 
@@ -11,17 +13,21 @@ class MainMenu extends Component {
                     mode="inline"
                     theme="dark">                    
                     <Menu.Item key="home"  >
-                        <Icon type="home"/>
-                        <span>Home</span>
+                        <NavLink to="/home">
+                            <Icon type="home"/>
+                            <span>Home</span>
+                        </NavLink>                        
                     </Menu.Item>
                     <Menu.Item key="transactions">
-                        <Icon type="table" />
-                        <span>Transações</span>
+                        <NavLink to="/transactions">
+                            <Icon type="table" />
+                            <span>Transações</span>
+                        </NavLink>                        
                     </Menu.Item>          
                 </Menu>                  
             </div>
         ) 
-    }
+    };
 }
 
 export default MainMenu;
