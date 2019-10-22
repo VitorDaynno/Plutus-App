90688c62ff1e13d90de52f19f2ed3b8bfece333a
VitorDaynno <vitordaynno@gmail.com>
2019-10-21 22:50:36 -0300

    Refatoração em processo

--- src/components/transaction/transaction.jsx
+++ src/components/transaction/transaction.jsx
@@ -15,10 +15,10 @@ import {
 } from 'antd';
 import axios from 'axios';
 import locale from 'antd/es/date-picker/locale/pt_BR';
-import moment from 'moment';
 
 
 const { Option } = Select;
+
 
 class Transaction extends Component {
   constructor(props) {
@@ -32,69 +32,9 @@ class Transaction extends Component {
     };
   }
 
-  componentDidMount() {
-    const { transaction } = this.props;
-
-    if(transaction.id){
-      this.setValues(transaction)
-    }
-
-    this.getAccounts();
-  }
-  
-  getAccounts() {
-    const token = localStorage.getItem('token');
-    const { history } = this.props;
-
-    axios.get('/v1/accounts', { headers: { Authorization: `Bearer ${token}` } } )
-      .then(res => this.setState({ accounts: res.data }))
-      .catch((error) => {
-        console.log(error);
-
-        if (error.response && error.response.status && error.response.status === 403) {
-          history.push('/');
-        }
-      });
-  }
-
-  setValues(transaction) {
-    const name = transaction.description;
-    const date = moment(transaction.purchaseDate);
-    const time = moment(transaction.purchaseDate);
-    const value = transaction.value;
-    const account = transaction.account.id;
-    const installmentsVisible = false;
-    const categories = transaction.categories;
-
-    this.setState({
-      name,
-      date,
-      time,
-      value,
-      account,
-      installmentsVisible,
-      categories
-    });
-  }
-
-  changeName = (e) => {
-    const name = e.target.value;
-    this.setState({ name });
-  }
-
-  changeDate = (e) => {
-    let date = e;
-    this.setState({ date });
-  }
-
   changeTime = (e) => {
     let time = e;
     this.setState({ time });
-  }
-
-  changeValue = (e) => {
-    let value = e;
-    this.setState({ value });
   }
 
   changeInstallments = (e) => {
@@ -190,12 +130,12 @@ class Transaction extends Component {
     const { history } = this.props;
     axios.post('/v1/transactions', transaction, { headers: { Authorization: `Bearer ${token}` } })
       .then(() => {
-        const { onClose } = this.props;
+        const { onClose, clearFields } = this.props;
         
         message.success('Transação salva com sucesso!');
         
         onClose();
-        this.clearFields()
+        clearFields(this);
       })
       .catch((error) => {
         message.error(error);
@@ -257,32 +197,33 @@ class Transaction extends Component {
   }
 
   render() {
-    const {
-      categories,
+    const { transaction, accounts, changeValue, changeDescription, changeDate } = this.props;
+
+    let {
       inputCategoryValue,
       inputCategoryVisible,
-      accounts,
       installmentsVisible,
-      name,
+      description,
       date,
       time,
       value,
       account,
-      installments
-    } = this.state;
-      
+      installments,
+      categories
+    } = transaction ? transaction : {};
+
     return (
       <div>
         <Row className="new-transaction-row">
           <Col span={24}>
-            <label className="label"> Nome:</label>
-            <Input onChange={this.changeName} value={name}/>
+            <label className="label">Nome:</label>
+            <Input value={description} onChange={changeDescription}/>
           </Col>
         </Row>
         <Row className="new-transaction-row">
           <Col className="date-group" lg={8} xs={24}>
             <label className="label">Data:</label>
-            <DatePicker format="DD/MM/YYYY" locale={locale} onChange={this.changeDate} value={date}/>
+            <DatePicker format="DD/MM/YYYY" locale={locale} onChange={changeDate} value={date}/>
           </Col>
           <Col className="date-group" lg={7} xs={24}>
             <label className="label">Horário:</label>
@@ -291,15 +232,15 @@ class Transaction extends Component {
           <Col lg={7} xs={24}>
             <label className="label">Valor:</label>
             <br />
-            <InputNumber className="max" precision={2} onChange={this.changeValue} value={value} decimalSeparator=","/>
+            <InputNumber className="max" precision={2} onChange={changeValue} value={value} decimalSeparator=","/>
           </Col>
         </Row>
         <Row className="new-transaction-row">
           <Col className="date-group" lg={11} xs={24}>
             <label className="label">Conta:</label>
             <br />
-            <Select className="max" onChange={this.changeAccount} value={account}>
-              {accounts.map(account => <Option value={account.id}>{account.name}</Option>)}
+            <Select className="max" onChange={this.changeAccount} value={account && account.id}>
+              {accounts.map(account => <Option value={account && account.id}>{account.name}</Option>)}
             </Select>
           </Col>
           {installmentsVisible && (<Col lg={11} xs={24}>
@@ -316,7 +257,7 @@ class Transaction extends Component {
           <Col>
             <label className="label">Categorias:</label>
             <br />
-            {categories.map((category) => {
+            {categories && categories.map((category) => {
               const isLongTag = category.length > 20;
               const tagElem = (
                 <Tag key={category} closable="true" afterClose={() => this.removeCategory(category)}>
--- src/components/transaction/transactionDrawer.jsx
+++ src/components/transaction/transactionDrawer.jsx
@@ -1,4 +1,5 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import {
   Drawer,
   Tabs,
@@ -19,10 +20,63 @@ const panels = [
   }
 ]
 
-class TransactionDrawer extends PureComponent { 
+class TransactionDrawer extends Component { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      accounts: []
+    };
+  }
 
-  render() {
-    const { visible, onClose, transaction, title, activeTab } = this.props;
+  componentDidMount() {
+    this.getAccounts();
+  }
+  
+  getAccounts() {
+    const token = localStorage.getItem('token');
+    const { history } = this.props;
+
+    axios.get('/v1/accounts', { headers: { Authorization: `Bearer ${token}` } } )
+      .then(res => this.setState({ accounts: res.data }))
+      .catch((error) => {
+        console.log(error);
+
+        if (error.response && error.response.status && error.response.status === 403) {
+          history.push('/');
+        }
+      });
+  }
+
+  changeDescription = (e) => {
+    let description = e.target.value;
+    const { transaction } = this.state;
+    transaction.description = description;
+    this.setState({ transaction });
+  }
+
+  changeDate = (e) => {
+    let date = e;
+    const { transaction } = this.state;
+    transaction.date = date;
+    this.setState({ date });
+  }
+
+  changeValue = (e) => {
+    let value = e;
+    const { transaction } = this.state;
+    transaction.value = value;
+    this.setState({ transaction });
+  }
+  
+  render() { 
+
+    if(this.props.transaction !== this.state.transaction) {
+      const transaction = this.props.transaction;
+      this.setState({transaction})
+    }
+
+    const { visible, onClose,  title, activeTab } = this.props ;
+    const { accounts, transaction } = this.state;
 
     return (
       <div>
@@ -38,7 +92,14 @@ class TransactionDrawer extends PureComponent {
             { panels.map((panel) => {
               return (
                 <TabPane tab={panel.title} key={panel.key}>
-                  <Transaction onClose={onClose} type={panel.key} transaction={transaction} />
+                  <Transaction 
+                    onClose={onClose} 
+                    type={panel.key} 
+                    transaction={transaction} 
+                    accounts={accounts}
+                    changeValue={this.changeValue}
+                    changeDescription={this.changeDescription}
+                    changeDate={this.changeDate}/>
                 </TabPane>
               )
             })}
